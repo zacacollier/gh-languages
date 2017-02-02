53aeb8786ccc6781ee5f5e5a2136c6eb6ab26476
Zac Collier <zacacollier@gmail.com>
2017-02-02 12:07:24 -0600

    GitHub Social Login working!

--- client/src/utils/AuthService.js
+++ client/src/utils/AuthService.js
@@ -1,44 +1,33 @@
-import { EventEmitter }   from 'events'
+import { EventEmitter } from 'events'
 import { isTokenExpired } from './jwtHelper'
-import Auth0Lock          from 'auth0-lock'
 import { browserHistory } from 'react-router'
-import auth0              from 'auth0-js'
+import auth0 from 'auth0-js'
 
 export default class AuthService extends EventEmitter {
   constructor(clientId, domain) {
     super()
-    this.domain = domain  // setting domain parameter as an instance attribute
     // Configure Auth0
     this.auth0 = new auth0.WebAuth({
-      clientID: 'ezsFdDPSYvu8EFCEU639Fy4gw7OVaOj9',
-      domain: 'zacacollier.auth0.com',
+      clientID: clientId,
+      domain: domain,
       responseType: 'token id_token',
-      redirectUri: 'http://localhost:3000/home'
+      redirectUri: 'https://zacacollier.auth0.com/login/callback'
+      // redirectUri: `${window.location.origin}/login`
     })
-    this.lock = new Auth0Lock(clientId, domain, {
-      auth: {
-        redirectUrl: `${window.location.origin}/login`,
-        responseType: 'token'
-      }
-    })
-    // Add callback for lock `authenticated` event
-    this.lock.on('authenticated', this._doAuthentication.bind(this))
-    // Add callback for lock `authorization_error` event
-    this.lock.on('authorization_error', this._authorizationError.bind(this))
-    // binds login functions to keep this context
+
     this.login = this.login.bind(this)
     this.signup = this.signup.bind(this)
     this.loginWithGitHub = this.loginWithGitHub.bind(this)
   }
 
-  login() {
+  login(username, password) {
     this.auth0.client.login({
       realm: 'Username-Password-Authentication',
       username,
       password
     }, (err, authResult) => {
       if (err) {
-        alert(`Error: ${err.description}`)
+        alert('Error: ' + err.description)
         return
       }
       if (authResult && authResult.idToken && authResult.accessToken) {
@@ -46,8 +35,6 @@ export default class AuthService extends EventEmitter {
         browserHistory.replace('/home')
       }
     })
-    // Call the show method to display the widget.
-    // this.lock.show()
   }
 
   signup(email, password){
@@ -55,8 +42,10 @@ export default class AuthService extends EventEmitter {
       connection: 'Username-Password-Authentication',
       email,
       password,
-    }, (err) => {
-      if (err) alert(`Error: ${err.description}`)
+    }, function(err) {
+      if (err) {
+        alert('Error: ' + err.description)
+      }
     })
   }
 
@@ -66,76 +55,57 @@ export default class AuthService extends EventEmitter {
     })
   }
 
-  _doAuthentication(authResult){
-    // Saves the user token
-    this.setToken(authResult.idToken)
-    // navigate to the home route
-    browserHistory.replace('/home')
-    // Async loads the user profile data
-    this.lock.getProfile(authResult.idToken, (error, profile) => {
-      if (error) {
-        console.log('Error loading the Profile', error)
-      } else {
-        this.setProfile(profile)
+  parseHash(hash) {
+    this.auth0.parseHash({ hash }, (err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setToken(authResult.accessToken, authResult.idToken)
+        browserHistory.replace('/home')
+        this.auth0.client.userInfo(authResult.accessToken, (error, profile) => {
+          if (error) {
+            console.log('Error loading the Profile', error)
+          } else {
+            this.setProfile(profile)
+          }
+        })
+      } else if (authResult && authResult.error) {
+        alert('Error: ' + authResult.error)
       }
     })
   }
 
-  _authorizationError(error){
-    // Unexpected authentication error
-    console.log('Authentication Error', error)
-  }
-
-
-  loggedIn(){
+  loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken()
     return !!token && !isTokenExpired(token)
   }
 
-  setProfile(profile){
+  setToken(accessToken, idToken) {
+    // Saves user access token and ID token into local storage
+    localStorage.setItem('access_token', accessToken)
+    localStorage.setItem('id_token', idToken)
+  }
+
+  setProfile(profile) {
     // Saves profile data to localStorage
     localStorage.setItem('profile', JSON.stringify(profile))
     // Triggers profile_updated event to update the UI
     this.emit('profile_updated', profile)
   }
 
-  getProfile(){
+  getProfile() {
     // Retrieves the profile data from localStorage
     const profile = localStorage.getItem('profile')
     return profile ? JSON.parse(localStorage.profile) : {}
   }
 
-  updateProfile(userId, data) {
-    const headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + this.getToken()
-    }
-    // Make PATCH request to auth0
-    return fetch(`https://${this.domain}/api/v2/users/${userId}`, {
-      method: 'PATCH',
-      headers: headers,
-      body: JSON.stringify(data)
-    })
-    .then(res => res.json())
-    .then(newProfile => this.setProfile(newProfile))
-    //TODO: persist profile updates in postgres
-  }
-
-  setToken(idToken){
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken)
-  }
-
-  getToken(){
+  getToken() {
     // Retrieves the user token from localStorage
     return localStorage.getItem('id_token')
   }
 
-  logout(){
+  logout() {
     // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('profile');
+    localStorage.removeItem('id_token')
+    localStorage.removeItem('profile')
   }
 }
